02069759fe4b488582aea08f1dd56aa528b0e87c
Scott Petersen <scott@digression.org>
2017-07-23 12:59:30 -0700

    Whitespace stripping.

--- Decorators/ConditionalDecorator.gd
+++ Decorators/ConditionalDecorator.gd
@@ -9,23 +9,23 @@ func _init(node, conditional, should_reevaluate=false):
 	self.node = node
 	self.conditional = conditional
 	self.should_reevaluate = should_reevaluate
-		
+
 func on_start():
 	conditional_status = TaskStatus.INVALID
-	
+
 func update(context, delta):
 	conditional_status = execute_conditional(context)
-	
+
 	if (conditional_status == TaskStatus.SUCCESS):
 		return child.tick(context, delta)
-	
+
 	return TaskStatus.FAILURE
-	
+
 func invalidate():
 	conditional_status = TaskStatus.INVALID
 
 func execute_conditional(context, force_update = false):
 	if force_update or should_reevaluate or conditional_status == TaskStatus.INVALID:
 		conditional_status = node.call(conditional, context)
-	
+
 	return conditional_status
